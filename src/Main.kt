c8bddbfc9aeed63521b939e6d6bf4b314d121afd
Aleksei Pavlov <a.pavlov@parity.digital>
2021-10-31 12:47:01 +0500

    bbb

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,14 @@
 fun main() {
 
     val a = readLine()!!.toInt()
-//    val b = readLine()!!.toInt()
-//    val c = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    val c = readLine()!!.toInt()
 
-    if (a > 0) {print("YES")}
-    else {print("NO")}
+    if (c > a && c < b) {
+        print("Normal")
+    } else if (c < a) {
+        print("Deficiency")
+    } else if (c > b) {
+        print("Excess")
+    }
 }
