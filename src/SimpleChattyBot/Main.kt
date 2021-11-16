bc6daeedebc98cef38bc480668526d6fe560cc4c
Aleksei Pavlov <a.pavlov@parity.digital>
2021-11-16 17:52:42 +0500

    final

--- src/SimpleChattyBot/Main.kt
+++ src/SimpleChattyBot/Main.kt
@@ -1,29 +1,21 @@
 package SimpleChattyBot
-
+import java.lang.Math
 
 
 fun main() {
-    val box1: Array<Int> = readLine()!!.split(" ").map{it.toInt()}.toTypedArray().sortedArray()
-    val box2: Array<Int> = readLine()!!.split(" ").map{it.toInt()}.toTypedArray().sortedArray()
-    if (box1[0] > box2[0] && box1[1] > box2[1] && box1[2] > box2[2]) {
-        println("Box 1 > Box 2")
-    } else if (box1[0] >= box2[0] && box1[1] >= box2[1] && box1[2] > box2[2]) {
-        println("Box 1 > Box 2")
-    } else if (box1[0] >= box2[0] && box1[1] > box2[1] && box1[2] >= box2[2]) {
-        println("Box 1 > Box 2")
-    } else if (box1[0] > box2[0] && box1[1] >= box2[1] && box1[2] >= box2[2]) {
-        println("Box 1 > Box 2")
-    } else if (box1[0] < box2[0] && box1[1] < box2[1] && box1[2] < box2[2]) {
-        println("Box 1 < Box 2")
-    } else if (box1[0] <= box2[0] && box1[1] <= box2[1] && box1[2] < box2[2]) {
-        println("Box 1 < Box 2")
-    } else if (box1[0] <= box2[0] && box1[1] < box2[1] && box1[2] <= box2[2]) {
-        println("Box 1 < Box 2")
-    } else if (box1[0] < box2[0] && box1[1] <= box2[1] && box1[2] <= box2[2]) {
-        println("Box 1 < Box 2")
-    } else if (box1[0] == box2[0] && box1[1] == box2[1] && box1[2] == box2[2]) {
-        println("Box 1 = Box 2")
-    } else {
-        println("Incomparable")
+    println("Let's test your programming knowledge.")
+    println("Why do we use methods?")
+    println("1. To repeat a statement multiple times.")
+    println("2. To decompose a program into several small subroutines.")
+    println("3. To determine the execution time of a program.")
+    println("4. To interrupt the execution of a program.")
+    var win = false
+    while (win == false) {
+        val answer = scanner.nextInt()
+        if (answer == 2) {
+            win = true
+        } else {
+            println("Please, try again.")
+        }
     }
 }
